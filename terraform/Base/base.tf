5b997814e37f7967fa431d913c97f72dd32ae061
Simon Rycroft <simon.rycroft@skybettingandgaming.com>
2019-03-05 16:57:42 +0000

    [refs hello-world] Removing AWS keys. They should come from ENV vars

--- terraform/Base/base.tf
+++ terraform/Base/base.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-  access_key = "${var.TF_VAR_aws_access_key_id}"
-  secret_key = "${var.TF_VAR_aws_secret_access_key}"
-  region     = "${var.aws_region}"
+  region = "${var.aws_region}"
 }
