c5eff1d7dd9e64945503908b85a78f87ff28f5c8
Simon Rycroft <simon.rycroft@skybettingandgaming.com>
2019-03-05 17:10:07 +0000

    [refs hello-world] Changed access keys

--- terraform/Base/base.tf
+++ terraform/Base/base.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "${var.TF_VAR_aws_access_key_id}"
-  secret_key = "${var.TF_VAR_aws_secret_access_key}"
+  access_key = "${var.aws_access_key_id}"
+  secret_key = "${var.aws_secret_access_key}"
   region     = "${var.aws_region}"
 }
