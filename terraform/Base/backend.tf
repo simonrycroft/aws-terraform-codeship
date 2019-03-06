01386809de75b61b8c5a403bdcde25fe93045064
Simon Rycroft <simon.rycroft@skybettingandgaming.com>
2019-03-06 11:49:39 +0000

    [refs hello-world] Manually created S3 bucket

--- terraform/Base/backend.tf
+++ terraform/Base/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "simonrycroft-terraform-practice"
+    bucket         = "0674-1131-1679-eu-west-2-terraform-state"
     key            = "shared/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
