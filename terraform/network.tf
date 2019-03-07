451de3a648e4025fb3a63cb6362ba20bd092373a
Simon Rycroft <simon.rycroft@skybettingandgaming.com>
2019-03-07 13:01:04 +0000

    [refs hello-world] Added name tags

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main"
+    Name = "Main"
   }
 }
 
@@ -15,6 +15,10 @@ resource "aws_subnet" "private" {
   cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id            = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "Private Subnets"
+  }
 }
 
 # Create var.az_count public subnets, each in a different AZ
@@ -24,9 +28,17 @@ resource "aws_subnet" "public" {
   availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id                  = "${aws_vpc.main.id}"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnets"
+  }
 }
 
 # IGW for the public subnet
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "Main Internet Gateway"
+  }
 }
