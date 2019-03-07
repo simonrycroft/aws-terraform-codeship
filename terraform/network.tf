0ecb66bf875cf0a5e5d85ff48cee89ae83d8866c
Simon Rycroft <simon.rycroft@skybettingandgaming.com>
2019-03-07 12:51:27 +0000

    [refs hello-world] Added public subnets

--- terraform/network.tf
+++ terraform/network.tf
@@ -16,3 +16,12 @@ resource "aws_subnet" "private" {
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id            = "${aws_vpc.main.id}"
 }
+
+# Create var.az_count public subnets, each in a different AZ
+resource "aws_subnet" "public" {
+  count                   = "${var.az_count}"
+  cidr_block              = "${cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)}"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  map_public_ip_on_launch = true
+}
