36e8230c444cb53245692f913c3ef1972ec26fb0
Simon Rycroft <simon.rycroft@skybettingandgaming.com>
2019-03-07 13:10:59 +0000

    [refs hello-world] Removed tags from route table

--- terraform/network.tf
+++ terraform/network.tf
@@ -48,8 +48,4 @@ resource "aws_route" "internet_access" {
   route_table_id         = "${aws_vpc.main.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.gw.id}"
-
-  tags = {
-    Name = "IG Public Routes"
-  }
 }
